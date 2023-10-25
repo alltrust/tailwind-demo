decba154483c091c517500d359e0c1cd2b856fcb
alltrust <aldorgarcia29@gmail.com>
2023-10-25 17:15:47 -0400

    customizations

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,16 @@ import Image from "next/image";
 
 //custom colours
 //config.ts
+
+//media queries apply via sm: , md: , lg: , xl: , 2xl: 
+
+//arbitrary values in []
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-custom_blue-000 text-white border-4 border-green-400">HELLO MUNDO!</h1>
-      <h1 className="text-yellow-600">HELLO MUNDO!</h1>
+      <h1 className="bg-custom_blue-000 border-green-400">HELLO MUNDO!</h1>
+      <h1 className="text-yellow-600 sm:text-sm md:text-lg xl:text-2xl">HELLO MUNDO!</h1>
+      <h1 className="text-[#bada50] sm:text-sm md:text-lg xl:text-2xl">HELLO MUNDO!</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,12 @@ import type { Config } from 'tailwindcss'
 
 //when you add custom colors, it overrides tailwind
 //therefore you can extend the custom colours as oppose to overriding
+
+//here we can modify and add the fontFamily
+
+//spacing refers to the margin, padding, etc. 
+
+//plugins
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,6 +15,20 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '100px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
     extend: {
       colors: {
         custom_blue:{
